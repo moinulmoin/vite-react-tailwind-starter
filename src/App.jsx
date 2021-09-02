0036eec4c1d9e085fbaa513d17a98158f9299668
Moinul Moin <moinulmoin101@gmail.com>
2021-09-02 11:27:04 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<div className='w-3/12 p-6 mx-auto rounded text-center bg-gray-900 space-y-5'>
+				<h1 className='text-white text-4xl'>
+					Vite + React + Tailwind
+				</h1>
+				<p className='text-blue-300 text-2xl'>It's working</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
