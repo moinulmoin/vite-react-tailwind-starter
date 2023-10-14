474518059d4c03e6cecaad85bf3d3e4ca16e4001
Moinul Moin <moinulmoin101@gmail.com>
2023-10-14 22:02:35 +0600

    chore: bump to latest versions of deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Vite, React, Tailwind, Shadcn UI minimal starter
+        Vite, React, Tailwind minimal starter
       </h1>
     </main>
   );
